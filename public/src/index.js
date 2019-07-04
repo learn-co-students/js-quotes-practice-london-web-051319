d28e65a0607428eaa62250e1da5063f58eea7817
AlisherZakir <a3zakir@gmail.com>
2019-07-04 20:05:21 +0100

    Done.

---
+++ public/src/index.js
@@ -0,0 +1,6 @@
+// It might be a good idea to add event listener to make sure this file
+// only runs after the DOM has finshed loading.
+
+document.addEventListener("DOMContentLoaded", () => {
+  makeBread();
+})
--- src/index.js
+++
@@ -1,2 +0,0 @@
-// It might be a good idea to add event listener to make sure this file 
-// only runs after the DOM has finshed loading. 
